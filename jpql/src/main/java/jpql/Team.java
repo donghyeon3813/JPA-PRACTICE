38b1ac43fb8c22bc2d7aa4b72f6bc43c7342f2c0
donghyeon3813 <ehdgusdl5753@gmail.com>
2024-04-01 20:06:15 +0900

    jpql 조인

--- jpql/src/main/java/jpql/Team.java
+++ jpql/src/main/java/jpql/Team.java
@@ -34,4 +34,14 @@ public class Team {
 
         this.name = name;
     }
+
+    public List<Member> getMembers() {
+
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+
+        this.members = members;
+    }
 }
